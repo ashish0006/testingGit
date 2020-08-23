a429bc89922f37e34ff1301477034d0b565a3cca
ashish0006 <cont2ashish006@gmail.com>
2020-08-23 13:51:53 +0530

    Added additional check

--- src/testingGit/TestGit2.java
+++ src/testingGit/TestGit2.java
@@ -3,6 +3,7 @@ package testingGit;
 public class TestGit2 {
 	public static void main(String[] args) {
 		System.out.println("Test Git 2");
+		System.out.println("Test Git 2 is checked again after reviewed by ashish");
 	}
 
 }
